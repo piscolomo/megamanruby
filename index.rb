8a05d072cd40e477c8a48ae247b011b375bb0108
Julio Lopez Montalvo <ljuliom@gmail.com>
2014-06-08 13:16:48 -0500

    megaman run

--- index.rb
+++ index.rb
@@ -11,8 +11,38 @@ class Game < Chingu::Window
 end
 
 class Megaman < Chingu::GameObject
+	def setup
+		@animations = {}
+		@state = :stand
+		@direction = :right
+		@last_x, @last_y = @x, @y
+	end
+
 	def initialize(options={})
 		super
+		self.input = [:holding_left, :holding_right]
+		@animations[:stand] = Chingu::Animation.new(:file => "standfull.png", :size => [31,24], :delay => 500)
+		@animations[:stand].frame_names = { :shootleft => 0, :left => 1..3, :right => 4..6, :shootright => 7}
+		@animations[:run] = Chingu::Animation.new(:file => "runfull.png", :size => [24,24], :delay => 200)
+		@animations[:run].frame_names = { :left => 0..3, :right => 4..7}
+	end
+
+	def update
+		@image = @animations[@state][@direction].next!
+		@state = :stand if @x == @last_x
+		@last_x, @last_y = @x, @y
+	end
+
+	def holding_left
+		@x -= 2
+		@state = :run
+		@direction = :left
+	end
+
+	def holding_right
+	  @x += 2
+	  @state = :run
+	  @direction = :right
 	end
 end
 
