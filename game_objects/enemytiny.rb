7c82157a8fe0407f1b9847cee6eb36c8838de90d
Julio López Montalvo <ljuliom@gmail.com>
2015-04-16 15:41:06 -0500

    Update enemytiny.rb

--- game_objects/enemytiny.rb
+++ game_objects/enemytiny.rb
@@ -1,32 +1,32 @@
 class EnemyTiny < GameObject
-	traits :bounding_box, :collision_detection, :velocity, :timer
-	attr_accessor :state, :power
+  traits :bounding_box, :collision_detection, :velocity, :timer
+  attr_accessor :state, :power
 
-	def setup
-		@animations= Animation.new(:file => "enemytiny.png", :size => [30,30], :delay => 400)
-		@animations.frame_names = { :stand=> 5..5, :walk => 0..5}
-		@state = :stand
-		@factor_x = 1
-		@image = @animations[@state].next
-		@zorder = 999999
-		@power = 2
-		@shooting = false
-		self.max_velocity = 10
+  def setup
+    @animations= Animation.new(:file => "enemytiny.png", :size => [30,30], :delay => 400)
+    @animations.frame_names = { :stand=> 5..5, :walk => 0..5}
+    @state = :stand
+    @factor_x = 1
+    @image = @animations[@state].next
+    @zorder = 999999
+    @power = 2
+    @shooting = false
+    self.max_velocity = 10
     self.acceleration_y = 0.5
-	end
+  end
 
-	def direction; @factor_x > 0 ? :left : :right; end
+  def direction; @factor_x > 0 ? :left : :right; end
 
-	def shoot
-		unless @shooting
-			BallRed.create(:x => @x, :y => @y-self.height/2, :direction => direction)
-			@shooting = true
-			after(1500){ @shooting = false }
-		end
-	end
+  def shoot
+    unless @shooting
+      BallRed.create(:x => @x, :y => @y-self.height/2, :direction => direction)
+      @shooting = true
+      after(1500){ @shooting = false }
+    end
+  end
 
-	def update
-		@image = @animations[@state].next
-		self.each_collision(Floor){ |enemytiny, floor| enemytiny.y = floor.bb.top - 15 }
-	end
+  def update
+    @image = @animations[@state].next
+    self.each_collision(Floor){ |enemytiny, floor| enemytiny.y = floor.bb.top - 15 }
+  end
 end
