e3a28c13bcf82c3bb722c1ab9da6647de6d63652
Julio López Montalvo <ljuliom@gmail.com>
2015-04-16 15:32:55 -0500

    Update intro.rb

--- game_states/intro.rb
+++ game_states/intro.rb
@@ -1,11 +1,11 @@
 class Intro < GameState
-	def initialize
-		super
-		#Sound["opening.wav"].play
-		self.input = {:return => Play}
-	end
-	def draw
-		super
-		Image["intro.jpg"].draw(0, 0, 0)
-	end
+  def initialize
+    super
+    #Sound["opening.wav"].play
+    self.input = {:return => Play}
+  end
+  def draw
+    super
+    Image["intro.jpg"].draw(0, 0, 0)
+  end
 end
