602ff6c11d7ba1ec18c37e7217dd3bf5b1bf2070
Julio López Montalvo <ljuliom@gmail.com>
2015-04-16 15:39:38 -0500

    Update enemyface.rb

--- game_objects/enemyface.rb
+++ game_objects/enemyface.rb
@@ -1,17 +1,17 @@
 class EnemyFace < GameObject
-	traits :bounding_box, :collision_detection
-	attr_accessor :state, :power
+  traits :bounding_box, :collision_detection
+  attr_accessor :state, :power
 
-	def setup
-		@animations= Animation.new(:file => "enemyface.png", :size => [46,48], :delay => 400)
-		@animations.frame_names = {:stand => 0..0, :walk => 0..3}
-		@state = :stand
-		@image = @animations[@state].next
-		@zorder = 999999
-		@power = 3
-	end
+  def setup
+    @animations= Animation.new(:file => "enemyface.png", :size => [46,48], :delay => 400)
+    @animations.frame_names = {:stand => 0..0, :walk => 0..3}
+    @state = :stand
+    @image = @animations[@state].next
+    @zorder = 999999
+    @power = 3
+  end
 
-	def update
-		@image = @animations[@state].next
-	end
+  def update
+    @image = @animations[@state].next
+  end
 end
