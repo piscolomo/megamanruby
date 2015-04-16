9c7eec8dacc90d881dfb9e1ab8a1b31c8958b8de
Julio López Montalvo <ljuliom@gmail.com>
2015-04-16 15:37:28 -0500

    Update play.rb

--- game_states/play.rb
+++ game_states/play.rb
@@ -1,17 +1,17 @@
 class Play < GameState
-	trait :viewport
-	def initialize
-		super
+  trait :viewport
+  def initialize
+    super
     destroy_instances
-		self.input = {:p => Pause, :e => :edit}
-		self.viewport.lag = 0
-		self.viewport.game_area = [0,0,5000,600]
-		load_game_objects
-		@parallax = Parallax.new(:x => 0, :y=>0, :rotation_center => :top_left)
+    self.input = {:p => Pause, :e => :edit}
+    self.viewport.lag = 0
+    self.viewport.game_area = [0,0,5000,600]
+    load_game_objects
+    @parallax = Parallax.new(:x => 0, :y=>0, :rotation_center => :top_left)
     @parallax << { :image => "background.jpg", :repeat_x => true, :repeat_y => true}
-		@megaman = Megaman.create(:x => 80, :y => 400)
-		@lifebar = Lifebar.create(:x => 30, :y => 100)
-	end
+    @megaman = Megaman.create(:x => 80, :y => 400)
+    @lifebar = Lifebar.create(:x => 30, :y => 100)
+  end
 
   #when a user decide to continue(from lose screen) chingu load for default instances of previous objects
   def destroy_instances
@@ -20,14 +20,14 @@ class Play < GameState
     EnemyFace.destroy_all
   end
 
-	def edit
+  def edit
     push_game_state(GameStates::Edit.new(:grid => [18,18], :classes => [EnemyFace, EnemyTiny, Floor, Lifeball]))
   end
 
   def draw
-  	super
-  	@lifebar.draw_position(self.viewport.x)
-  	@parallax.draw
+    super
+    @lifebar.draw_position(self.viewport.x)
+    @parallax.draw
   end
 
   #make the EnemyFace follow megaman
@@ -50,8 +50,8 @@ class Play < GameState
     end
   end
 
-	def update
-		super
+  def update
+    super
 
     #lose the game if your life is 0
     push_game_state(Lose) if @lifebar.health == 0
@@ -60,9 +60,9 @@ class Play < GameState
     push_game_state(Lose) if @megaman.y > $window.height
 
     #move the background
-		self.viewport.center_around(@megaman)
-		@parallax.camera_x, @parallax.camera_y = self.viewport.x.to_i, self.viewport.y.to_i
-		@parallax.update
+    self.viewport.center_around(@megaman)
+    @parallax.camera_x, @parallax.camera_y = self.viewport.x.to_i, self.viewport.y.to_i
+    @parallax.update
 
     #balls are destroyed if they are outside the view
     Ball.all.each {|ball| ball.destroy unless self.viewport.inside?(ball)}
@@ -74,13 +74,13 @@ class Play < GameState
 
     #destroy EnemyFace and ball if collision  
     Ball.each_collision(EnemyFace, EnemyTiny) do |ball, enemy|
-	    enemy.destroy
-	    ball.destroy
+      enemy.destroy
+      ball.destroy
     end
 
     #down life if megaman collision with EnemyFace
     @megaman.each_collision(EnemyFace, EnemyTiny, BallRed) do |me, enemy|
-    	me.take_damage
+      me.take_damage
       @lifebar.downlife(enemy.power)
     end
 
@@ -89,5 +89,5 @@ class Play < GameState
       @lifebar.uplife
       lifeball.destroy
     end
-	end
+  end
 end
